5013dd8747acf535007c817a032e259f6ce15f18
Omer Texerman <omer@texerman.com>
2026-03-19 00:24:55 +0000

    added linked in

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import { siteOrigin } from "./src/config.ts";
 
 export default defineConfig({
 	site: siteOrigin,
 	output: "static",
 	adapter: vercel(),
-	integrations: [sitemap()],
+	integrations: [sitemap(), icon()],
 	vite: {
 		plugins: [tailwindcss()],
 		optimizeDeps: {
